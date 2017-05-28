d2942da4d14be15b082907ff914965e9d4fd379b
goroh83 <goroh83@yahoo.com>
2017-05-28 14:38:02 +0200

    generateRandomNumber function added

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector('#colorDisplay');
 var messageDisplay = document.querySelector('#message');
 
@@ -29,7 +22,6 @@ for (var i = 0; i < squares.length; i++) {
             this.style.backgroundColor = "#293249";
             messageDisplay.textContent = "Try Again!";
         }
-
     });
 }
 
@@ -37,4 +29,27 @@ function changeColors(color) {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor =  color;
     }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length)
+    return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r  + ", " + g + ", " + b + ")"
 }
